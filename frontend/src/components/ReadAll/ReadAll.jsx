f431ed1696dac4cbe4eabdcce0c87069f0586e61
Thais Tauane Cardoso <65881471+thaistcardoso@users.noreply.github.com>
2023-02-24 10:58:16 -0300

    atualizando url

--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -60,7 +60,7 @@ function ReadAll() {
   // Realizar requisição para backend obtendo a lista de itens
   async function realizarRequisicao() {
     // const url = "http://localhost:3000/item";
-    const url = "https://ocean-jornada-fullstack-thaistcardoso.onrender.com";
+    const url = "https://ocean-jornada-fullstack-thaistcardoso.onrender.com/item";
     const response = await fetch(url);
     const data = await response.json();
 
